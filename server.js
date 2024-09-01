7796ffb5a83573ce2d31832489d1ba586757c43f
freyako-ga <freyako96@gmail.com>
2024-09-01 22:44:24 +0100

    deploy try

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv')
-dotenv.config()
+require('dotenv').config();
+// dotenv.config()
 const cors = require('cors')
 const express = require('express')
 const app = express()
